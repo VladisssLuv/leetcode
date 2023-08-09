ffc044893088a61d1c408123b5bfe4dbc4e47da5
VladisssLuv <vladissslav19@gmail.com>
2023-08-09 22:01:44 +0300

    add: 136. Single Number

---
+++ kotlinSolutions/src/main/kotlin/easy/SingleNumber.kt
@@ -0,0 +1,11 @@
+package easy
+
+class SingleNumber {
+    fun singleNumber(nums: IntArray): Int {
+        var res = 0
+         for (num in nums) {
+             res = res.xor(num)
+         }
+        return res
+    }
+}
