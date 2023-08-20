0e981ac5a8ad3fbfa87c7fbbbd40c24c8d9f0396
VladisssLuv <vladissslav19@gmail.com>
2023-08-20 19:43:16 +0300

    add: 206. Reverse Linked List

---
+++ kotlinSolutions/src/main/kotlin/easy/ReverseLinkedList.kt
@@ -0,0 +1,30 @@
+package easy
+
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Stack
+
+class ReverseLinkedList {
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+    private val resultList = ListNode(0)
+    var iterator = resultList
+
+    fun reverseList(head: ListNode?): ListNode? {
+
+        helper(head)
+        return resultList.next
+    }
+
+    private fun helper(node: ListNode?) {
+        if(node == null) {
+            return
+        }
+
+        helper(node.next)
+        iterator.next = ListNode(node.`val`)
+        iterator = iterator.next!!
+    }
+
+}
